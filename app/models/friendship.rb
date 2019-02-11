7b1b78a6f4ead7d0265123cda6b2b68b5fa1eccc
zlatanowsky <amaratc@gmail.com>
2019-02-11 13:40:19 +0100

    Added bidirectional association

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,27 @@
 class Friendship < ApplicationRecord
     belongs_to :international
-    belongs_to :friend, :class_name => 'International'
+    belongs_to :friend, class_name: 'International'
+
+    after_create :create_inverse, unless: :has_inverse?
+    after_destroy :destroy_inverses, if: :has_inverse?
+
+    def create_inverse
+      self.class.create(inverse_friendship_options)        
+    end
+
+    def destroy_inverse
+      inverse.destroy_all
+    end
+
+    def has_inverse?
+      self.class.exists?(inverse_friendship_options)      
+    end
+
+    def inverse
+      self.class.where(inverse_friendship_options)      
+    end
+
+    def inverse_friendship_options
+      { friend_id: international_id, international_id: friend_id  }      
+    end
 end
--- app/models/international.rb
+++ app/models/international.rb
@@ -1,8 +1,13 @@
 class International < ApplicationRecord
     has_many :friendships
-    has_many :friends, :through => :friendships
-    has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-    has_many :inverse_friends, :through => :inverse_friendships, :source => :international
+    has_many :friends, :through => :friendships, dependent: :destroy
+    # has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+    # has_many :inverse_friends, :through => :inverse_friendships, :source => :international
 
     validates :name, :country, presence: true 
+
+    def country_name
+        country = ISO3166::Country[country_code]
+        country.translations[I18n.locale.to_s] || country.name
+    end
 end
